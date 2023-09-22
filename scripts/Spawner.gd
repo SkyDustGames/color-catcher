b793e112eb635c7125dd9ed00e13f06a42ee1e0c
SkyDustGames <thiiagoo912@hotmail.com>
2023-09-22 15:15:17 -0300

    Convert basic functionality to Godot

---
+++ scripts/Spawner.gd
@@ -0,0 +1,20 @@
+extends Timer
+
+var shapes = [ preload("res://nodes/square.tscn") ]
+@export var time_decrease: float
+@export var min_time: float
+
+func _ready():
+	randomize()
+
+func _on_timeout():
+	var shape_kind = shapes[randi() % shapes.size()]
+	var shape = shape_kind.instantiate()
+	shape.position = Vector2(randi_range(0, 1200), -100)
+	add_child(shape)
+	
+	var t = wait_time - time_decrease
+	if t <= min_time:
+		wait_time = min_time
+	else:
+		wait_time = t
